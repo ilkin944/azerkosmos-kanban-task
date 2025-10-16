393b03eaaf8ff57a16b7aa5c788bc44166d76884
Ilkin Zilfiyev <60821061+ilkin944@users.noreply.github.com>
2025-10-16 15:11:40 +0400

    Sorry for forgetting uncomment MainBoard.tsx 59-60'th lines

--- src/components/kanban/MainBoard.tsx
+++ src/components/kanban/MainBoard.tsx
@@ -56,8 +56,8 @@ export default function MainBoard() {
         <CardContent>
           <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
             <TodoTasks  />
-            {/* <InProgressTasks  /> */}
-            {/* <DoneTasks  /> */}
+            <InProgressTasks  />
+            <DoneTasks  />
           </div>
         </CardContent>
       </Card>
